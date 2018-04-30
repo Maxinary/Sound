113d75b6f0505dc17cf372b8dc6f7dc46cdf59cb
Maximilian Victor Scribner <max.scribs@gmail.com>
2018-04-30 00:28:58 -0400

    velocity

--- main.js
+++ main.js
@@ -7,14 +7,12 @@ var air = [];
 var airbuffer = [];
 var SIZE = 128;
 
-function length(a){
-	return Math.sqrt(a[0]*a[0]+a[1]*a[1]);
-}
+var wavePropogationSpeed = 1/6;
+var friction = 1;
 
 function sigmoid(x){
 	return 1/(1+Math.pow(2, -x))
 }
-
 
 function draw(){
 	var ratio = canvas.width/SIZE;
@@ -34,7 +32,11 @@ function hex(r,g,b){
 	if(g<0)	  g = 0;
 	if(b>255) b = 255;
 	if(b<0)	  b = 0;
-	return "#"+Math.floor(r).toString(16).padStart(2, "0")+Math.floor(g).toString(16).padStart(2, "0")+Math.floor(b).toString(16).padStart(2, "0");
+	return "#"+twoDigitHex(r)+twoDigitHex(g)+twoDigitHex(b);
+}
+
+function twoDigitHex(x){
+	return Math.floor(x).toString(16).padStart(2, "0");
 }
 
 function bound(x){
@@ -49,31 +51,27 @@ function bound(x){
 
 function step(){
 	airbuffer = air.map(function(e){return e.map(function(f){return f.slice();})});
-//	air = air.map(function(e){return e.map(function(f){return f.map(function(g){return 0;});});});
 	for(var i=0; i<air.length; i++){
 		for(var j=0; j<air[i].length; j++){
 			var surrounding = [];
 			var total = 0;
 			for(var v=bound(i-1); v<=bound(i+1); v++){
 				for(var h=bound(j-1); h<=bound(j+1); h++){
-					var dif = airbuffer[i][j][0]-airbuffer[v][h][0];
-					surrounding[(v-i+1)*3+(h-j+1)] = dif;
-					total += Math.abs(dif);
-				}
-			}
-			if(total != 0){
-				for(var v=bound(i-1); v<=bound(i+1); v++){
-					for(var h=bound(j-1); h<=bound(j+1); h++){
-						air[v][h][1] += airbuffer[i][j][0]*surrounding[(v-i+1)*3+(h-j+1)]/total;
+					var dif = (airbuffer[i][j][0]-airbuffer[v][h][0])/9;
+					if(Math.abs(i-v)+Math.abs(j-h) == 2){
+						dif /= Math.sqrt(2);
 					}
+					air[v][h][1] += wavePropogationSpeed*dif;
+					total += dif;
 				}
-				air[i][j][1] -= airbuffer[i][j][0];
 			}
+			air[i][j][1] -= wavePropogationSpeed*total;
 		}
 	}
 	for(var i=0; i<air.length; i++){
 		for(var j=0; j<air[i].length; j++){
-			air[i][j][0] += air[i][j][1];	
+			air[i][j][0] += air[i][j][1];
+			air[i][j][1] *= friction;
 		}
 	}
 }
